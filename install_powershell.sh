e00e35f3420027904bdb60270940a1ca994c02e7
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2018-10-17 15:42:03 +0300

    Update install_powershell.sh

--- install_powershell.sh
+++ install_powershell.sh
@@ -1,5 +1,12 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
+
+#download package
 sudo wget https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+
+#install package
 sudo dpkg -i packages-microsoft-prod.deb
+
 sudo apt-get update
 sudo apt-get install -y powershell
+sudp apt-get clean
+rm packages-microsoft-prod.deb
