4af1af92e55cb4b6e6af22b5de84f8873df5e687
Yaroslav <yorimark@abv.bg>
2018-10-17 15:28:28 +0300

    update travis_fina

--- install_powershell.sh
+++ install_powershell.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
-wget https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
-apt-get update
-apt-get install -y powershell
+sudo wget https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install -y powershell
 
